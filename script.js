2659379a91cb30739959a822593644104e830dfa
Jay <sighhype@naver.com>
2023-08-15 14:04:43 +0900

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hello JavaScript");
+
+var age = 25;
+let year = 2023;
+const point = 250;
+
+console.log(age);
+console.log(year);
+console.log(point);
+
+const today = "광복절";
+
+const pass = true;
+
+if (pass) {
+  console.log("hello");
+}
